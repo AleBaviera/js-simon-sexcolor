22f2a690d8a4a6dafd1f10ff20fdf0e8d9083418
alebaviera <baviera.alessandra@gmailcom>
2019-07-15 14:17:44 +0200

    fine

---
+++ sexcolor/main.js
@@ -0,0 +1,20 @@
+//
+// 2 -  Il software deve chiedere all’utente il suo nome e il sesso con due prompt diversi.
+// Sulla pagina html deve apparire “Ciao <nome>“, il colore del nome deve essere azzurro o rosa a seconda del sesso inserito;
+
+
+var nome = prompt('ti chiami?');
+
+var sex = prompt('sei m o f?');
+
+var element = document.getElementById('ciao');
+
+if (sex == 'm'){
+  element.style.color = 'lightblue';
+}
+
+else{
+  element.style.color = 'pink';
+}
+
+element.innerHTML = 'ciao ' + nome;
